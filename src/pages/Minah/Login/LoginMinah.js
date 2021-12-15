44a3030feed0a4221d7543ce38c2bde4e4447480
minami-cs <mina9301@gmail.com>
2021-12-15 17:51:39 +0900

    Add: LoginMinah.js와 LoginMinah.scss 추가 및 sass 라이브러리 설치

--- src/pages/Minah/Login/LoginMinah.js
+++ src/pages/Minah/Login/LoginMinah.js
@@ -1,3 +1,66 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Footer from "../../../components/Footer/Footer";
+import './Login.scss';
+
 export default function LoginMinah() {
-  
+    const [userId, setUserId] = useState('');
+    const [userPw, setUserPw] = useState('');
+
+    const onChangeUserId = (e) => {
+        setUserId(e.target.value);
+    }
+
+    const onChangeUserPw = (e) => {
+        setUserPw(e.target.value);
+    }
+
+    const navigate = useNavigate();
+    const goToMain = () => {
+        navigate('/main');
+    }
+
+    const onReset = () => {
+        setUserId('');
+        setUserPw('');
+        goToMain();
+    }
+
+    return (
+        <div className="login">
+            <main>
+                <div className="login_box">
+                    <h1>Westagram</h1>
+                    <form>
+                        <div className="login_input">
+                            <input type="text" id="user_id" placeholder="전화번호, 사용자 이름 또는 이메일" onChange={onChangeUserId} value={userId} />
+                            <label htmlFor="user_id">전화번호, 사용자 이름 또는 이메일</label>
+                        </div>
+                        <div className="login_input">
+                            <input type="password" id="user_pw" placeholder="비밀번호" onChange={onChangeUserPw} value={userPw} />
+                            <label htmlFor="user_pw">비밀번호</label>
+                        </div>
+                        <button type="button" onClick={onReset} disabled={(userId.includes('@') && userPw.length > 5) ? false : true
+                        }>로그인</button>
+                    </form>
+                    <div className="line">
+                        <span>또는</span>
+                    </div>
+                    <div className="facebook">
+                        <a href="https://www.facebook.com/">
+                            <img alt="페이스북 아이콘" src="images/facebook.png" />
+                            <span>Facebook으로 로그인</span>
+                        </a>
+                    </div>
+                    <div className="find_pw">
+                        <a href="https://www.instagram.com/accounts/password/reset/">비밀번호를 잊으셨나요?</a>
+                    </div>
+                </div>
+                <div className="go_to_join">
+                    <p>계정이 없으신가요? <a href="https://www.instagram.com/accounts/emailsignup/" target="_self">가입하기</a></p>
+                </div>
+            </main >
+            <Footer />
+        </div >
+    );
 }
