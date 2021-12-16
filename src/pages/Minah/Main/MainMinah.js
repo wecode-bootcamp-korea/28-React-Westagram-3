efbec6c4b088232cc79cc7f3e70d66f5506a0525
minami-cs <mina9301@gmail.com>
2021-12-16 20:47:17 +0900

    Add: Main 페이지용 각 컴포넌트 생성 및 scss 파일 생성 - Main 페이지 컴포넌트: Aside, Feed, NavigationBar, Story 파일 생성 완료 - Main.scss 생성 완료

--- src/pages/Minah/Main/MainMinah.js
+++ src/pages/Minah/Main/MainMinah.js
@@ -1,3 +1,21 @@
+import Aside from "./Aside/Aside";
+import Feed from "./Feed/Feed";
+import NavigationBar from "./NavigationBar/NavigationBar";
+import Story from "./Story/Story";
+
 export default function MainMinah() {
-  
+  return (
+    <div className="main">
+      <NavigationBar />
+      <main>
+        <section>
+          <Story />
+          <Feed />
+        </section>
+        <aside>
+          <Aside />
+        </aside>
+      </main>
+    </div>
+  );
 }
---
+++ src/pages/Minah/Main/Story/Story.js
@@ -0,0 +1,60 @@
+export default function Story() {
+  return (
+      <div className="story_box">
+          <div className="story_wrap">
+              <img src="images/Minah/Main/left-chevron.png" className="story_prev btn_style" alt="이전" />
+              <img src="images/Minah/Main/right-chevron.png" className="story_next btn_style" alt="다음" />
+              <ul className="story">
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/profile.jpg" className="story_profile" alt="내 스토리" />
+                      </div>
+                      <p className="story_old_color">Your story</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_new_border">
+                          <img src="images/Minah/Main/profile1.jpg" className="story_profile" alt="이흥용 과자점 스토리" />
+                      </div>
+                      <p className="story_new_color">lee_heung_yong_cake</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/cat.jpg" className="story_profile" alt="고양이러버 스토리" />
+                      </div>
+                      <p className="story_old_color">cat_lover</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/coffee.jpg" className="story_profile" alt="카페 스토리" />
+                      </div>
+                      <p className="story_old_color">cafe_knotted</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/profile2.jpg" className="story_profile" alt="여행 스토리" />
+                      </div>
+                      <p className="story_old_color">crazy_for_travel</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/starbucks.jpg" className="story_profile" alt="스타벅스 스토리" />
+                      </div>
+                      <p className="story_old_color">starbucks_korea</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/stationary.jpg" className="story_profile" alt="문구 스토리" />
+                      </div>
+                      <p className="story_old_color">stationary_store</p>
+                  </li>
+                  <li className="story_list">
+                      <div className="story_border story_old_border">
+                          <img src="images/Minah/Main/macbook.jpg" className="story_profile" alt="애플 스토리" />
+                      </div>
+                      <p className="story_old_color">apple_store_kr</p>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  );
+}
