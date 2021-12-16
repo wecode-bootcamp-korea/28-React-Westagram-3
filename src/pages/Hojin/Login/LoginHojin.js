2d44c04d5887361d47d9057425906c3d46e76cce
cil04105 <hojin6417@gmail.com>
2021-12-16 13:56:16 +0900

    [Add]Login 화면 -사용자 입렵 데이터 저장 기능 구현

--- src/pages/Hojin/Login/LoginHojin.js
+++ src/pages/Hojin/Login/LoginHojin.js
@@ -1,3 +1,83 @@
-export default function LoginHojin() {
-  
+// export default function LoginHojin() {
+
+// }
+import React from 'react';
+// import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './LoginHojin.scss';
+// const [pageMove, setPageMove] = useState('');
+
+function LoginHojin() {
+  const onChange = e => {
+    console.log(e.target.value);
+  };
+  return (
+    <div className="container">
+      <h1>Westagram</h1>
+      <div className="form">
+        <input
+          type="text"
+          id="inputId"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          id="inputPw"
+          placeholder="비밀번호"
+          onChange={onChange}
+        />
+        <Link to="/MainHojin">
+          <button>로그인</button>
+        </Link>
+      </div>
+      <div className="pwSearch">
+        <span>비밀번호를 잊으셨나요?</span>
+      </div>
+    </div>
+  );
+}
+
+export default LoginHojin;
+
+{
+  /* <div className="container">
+      <div className="header">
+        <h1>
+          <span className="topLogo" />
+          <span>Westagram</span>
+        </h1>
+      </div>
+
+      <div className="form">
+        <div className="inputBoxId">
+          <input
+            className="input_id"
+            id="id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            // onChange={updateEmail}
+          />
+        </div>
+
+        <div className="inputBoxPw">
+          <input
+            className="input_pw"
+            id="pw"
+            type="password"
+            placeholder="비밀번호"
+          />
+        </div>
+
+        <div className="buttonBox">
+          <Link to="/main">
+            <button className="btnOn btnOff">로그인</button>
+          </Link>
+        </div>
+
+        <div className="searchPwd">
+          <span>비밀번호를 잊으셨나요?</span>
+        </div>
+      </div>
+    </div> */
 }
--- src/pages/Hojin/Main/MainHojin.js
+++ src/pages/Hojin/Main/MainHojin.js
@@ -1,3 +1,48 @@
-export default function MainHojin() {
-  
+// export default function MainHojin() {
+
+// }
+import React from 'react';
+// import './MainHojin.scss';
+// import Feedimg from './images/Hojin/feedimg.jpeg';
+
+function MainHojin() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>이미지 | Westagram</h1>
+        <div className="navBar">
+          <ul>
+            <li>검색</li>
+            <li>하트</li>
+            <li>마이</li>
+          </ul>
+        </div>
+      </div>
+      <div className="main">
+        <article>
+          <h2>insta id</h2>
+          <img src="/images/Hojin/feedimg.jpeg" />
+          {/* <img src={Feedimg} /> */}
+          <section className="heartBtn">
+            <ul>
+              <li>검색</li>
+              <li>하트</li>
+              <li>마이</li>
+            </ul>
+            1명이 좋아함
+          </section>
+          <div className="coments">
+            <ul>
+              <li>gkgk</li>
+              <li>gkgk</li>
+            </ul>
+          </div>
+          <input type="text" id="commentBox" placeholder="댓글달기..." />
+          <button>게시</button>
+        </article>
+      </div>
+      <div className="main-side">fdafdsa</div>
+    </div>
+  );
 }
+export default MainHojin;
