5e0fe79ce0cba5422af1bb7b59a8bd8461bdc23e
minami-cs <mina9301@gmail.com>
2021-12-16 11:05:07 +0900

    Add: LoginMinah.js와 LoginMinah.scss 파일 추가 - LoginMinah.js 파일 추가 - LoginMinah.scss 파일 추가 후 공통 scss파일 임포트 완료

--- src/pages/Minah/Login/LoginMinah.js
+++ src/pages/Minah/Login/LoginMinah.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../../../components/Footer/Footer";
-import './Login.scss';
+import './LoginMinah.scss';
 
 export default function LoginMinah() {
     const [userId, setUserId] = useState('');
