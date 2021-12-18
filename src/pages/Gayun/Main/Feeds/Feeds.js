d16cf1e9f587b4b61754699991e4d3b224c774bf
gy <gayunlee11@gmail.com>
2021-12-18 22:41:36 +0900

    [Add] Feeds: feed.json 비동기로 받아오기

--- src/pages/Gayun/Main/Feeds/Feeds.js
+++ src/pages/Gayun/Main/Feeds/Feeds.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Feed from '../Feed/Feed';
-import feedsData from '../../data/feed.json';
 import { v4 as uuid } from 'uuid';
 import './Feeds.scss';
 
 function Feeds() {
+  const [feeds, setFeeds] = useState();
+  let data;
+
+  async function loadFeedData() {
+    data = await await (
+      await fetch('http://localhost:3001/data/feed.json')
+    ).json();
+    await setFeeds(data);
+  }
+
+  useEffect(() => {
+    loadFeedData();
+  });
+
   return (
     <div className="feeds">
-      {feedsData.map(feed => (
-        <Feed key={uuid()} feed={feed} />
-      ))}
+      {feeds && feeds.map(feed => <Feed key={uuid()} feed={feed} />)}
     </div>
   );
 }
