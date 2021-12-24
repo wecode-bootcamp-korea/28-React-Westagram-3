749cedde0923857b1262c87d4efb4435cea8b207
gy <gayunlee11@gmail.com>
2021-12-24 09:18:19 +0900

    [Refactor] Feeds: 무한 스크롤 기능 리팩토링 중 - state 수정 : isLoading, page, feedList - page 값을 기준으로 fetch하도록 useEffect()내부 setIsLoading, fetchFeedData() 추가 - fetchFeedData() 안에서 page값에 따른 데이터를 불러오도록 변경

--- src/pages/Gayun/Main/Feeds/Feeds.js
+++ src/pages/Gayun/Main/Feeds/Feeds.js
@@ -5,18 +5,13 @@ import Skeleton from '../Skeleton/Skeleton';
 import UseIntersected from './UseInfiniteScroll';
 import UseInfiniteScroll from './UseInfiniteScroll';
 import LoadMoreFeed from '../LoadMoreFeed/LoadMoreFeed';
+import { COMPARISON_BINARY_OPERATORS } from '@babel/types';
 
 function Feeds() {
   const [page, setPage] = useState(0);
   const [feedList, setFeedList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const [feeds, setFeeds] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [isLoadedMore, setIsLoadedMore] = useState(true);
-  const feedEndRef = useRef();
   const SKELETON_FEED_COUNT = 5;
-  let loadFeedNum = 0;
 
   function createSkeletonFeed(count) {
     let skeletonFeedCount = Array.from({ length: count }, (v, i) => i);
@@ -27,43 +22,21 @@ function Feeds() {
 
   useEffect(async () => {
     setIsLoading(true);
-    const list = await (await fetch(`data/Gayun/feed0.json`)).json();
-    setFeedList(prev => [...prev, ...list]);
+    fetchFeedData(page);
     setIsLoading(false);
   }, [page]);
 
-  async function loadFeedData(num) {
-    const data = await (await fetch(`data/Gayun/feed${num}.json`)).json();
-    setTimeout(() => {
-      setFeeds(data);
-      setIsLoaded(() => {
-        return true;
-      });
-    }, 2000);
+  async function fetchFeedData(page) {
+    const newFeeds = await (await fetch(`data/Gayun/feed${page}.json`)).json();
+    setFeedList(prevFeeds => [...prevFeeds, ...newFeeds]);
   }
-
-  async function loadMoreFeed(num) {
-    const data = await (await fetch(`data/Gayun/feed${num}.json`)).json();
-    setTimeout(() => {
-      setFeeds(feeds => {
-        return feeds.concat(data);
-      });
-    });
-    setIsLoadedMore(() => {
-      return true;
-    });
-  }
-
-  useEffect(() => {
-    loadFeedData(0);
-  }, []);
 
   return (
     <div className="feeds-gayun">
       <div className="feed-container">
-        {isLoaded
-          ? feeds.map((feed, idx) => <Feed key={idx} feed={feed} />)
-          : createSkeletonFeed(SKELETON_FEED_COUNT)}
+        {isLoading
+          ? createSkeletonFeed(SKELETON_FEED_COUNT)
+          : feedList.map((feed, idx) => <Feed key={idx} feed={feed} />)}
       </div>
       <LoadMoreFeed isLoading={page !== 0 && isLoading} setPage={setPage} />
     </div>
